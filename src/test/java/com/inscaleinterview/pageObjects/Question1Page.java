83083bcfac1a04022f5bfa75eea844d2fad25db5
Sarah Nafisa <saraa.nafisa@gmail.com>
2023-05-14 15:35:24 +0800

    First Push

--- src/test/java/com/inscaleinterview/pageObjects/Question1Page.java
+++ src/test/java/com/inscaleinterview/pageObjects/Question1Page.java
@@ -23,7 +23,6 @@ public class Question1Page extends BaseClass{
         PageFactory.initElements(rdriver, this);
     }
 
-	//test
     @FindBy(xpath = "//button[normalize-space()='Bank Manager Login']")
     WebElement bankManagerLogin;
 
